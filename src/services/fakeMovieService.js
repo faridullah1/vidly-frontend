f6a547fc7aa61521f7887ceff59d8742fe4eff21
Farid ullah <faridullah996@gmail.com>
2022-09-13 15:43:49 +0500

    movie component

---
+++ src/services/fakeMovieService.js
@@ -0,0 +1,31 @@
+const movies = [
+	{
+		_id: "62188020f5bfa29914a13b22",
+		title: 'Terminator',
+		genre: { _id: "62188020f5bfa29914a13b1e", name: 'Action' },
+		numberInStock: 6,
+		dailyRentalRate: 100
+	},
+	{
+		_id: "62188020f5bfa29914a1cu22",
+		title: 'Die Hard',
+		genre: { _id: "62188020f5bfa29914a13b1e", name: 'Action' },
+		numberInStock: 2,
+		dailyRentalRate: 150
+	},
+	{
+		_id: "62188020f5bfa29914adbb22",
+		title: '3 Idios',
+		genre: { _id: "62188020f5bfa29914a13b1e", name: 'Action' },
+		numberInStock: 20,
+		dailyRentalRate: 80
+	}
+];
+
+export function getMovies() {
+	return movies;
+}
+
+export function getMovie(id) {
+	return movies.find(m => m._id === id);
+}
