6685c59494fb2fe643b06251b3a9a179495e3ab7
Farid ullah <faridullah996@gmail.com>
2022-09-14 16:29:22 +0500

    prop types

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 const Pagination = (props) => {
 	const { totalItems, pageSize, currentPage, onPageChange } = props;
-	
+
 	const pagesCount = Math.ceil(totalItems / pageSize);
 	if (pagesCount === 1) return null;
 
@@ -20,6 +21,13 @@ const Pagination = (props) => {
 			</ul>
 		</nav>
 	);
+}
+
+Pagination.propTypes = {
+	totalItems: PropTypes.number.isRequired, 
+	pageSize: PropTypes.number.isRequired, 
+	currentPage: PropTypes.number.isRequired, 
+	onPageChange: PropTypes.func.isRequired
 }
  
 export default Pagination;
